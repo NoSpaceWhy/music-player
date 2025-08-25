30a7d7ed1cb0f6385f2888e6eb4a48dce61cf294
Space <ashrayvarughese@gmail.com>
2025-08-25 19:45:37 +0530

    first song and play button

--- main.py
+++ main.py
@@ -7,19 +7,28 @@ screen_w,screen_h = 800, 600
 screen = pygame.display.set_mode((screen_w, screen_h))
 
 # image load
+# music paly button
 play_button = pygame.image.load("assets/play.png").convert_alpha()
 play_button = pygame.transform.scale(play_button, (90, 80))  # Scale the image to desired size
 play_button_rect = play_button.get_rect(x=100, y=100 )
+
+# music load
+loaded_music = pygame.mixer.Sound("music/test.mp3")
 
 running = True
 while running:   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if play_button_rect.collidepoint(event.pos):
+                loaded_music.play(-1)
+            
     mos_pos = pygame.mouse.get_pos()
-    
     screen.fill((250, 238, 238))  # Fill the screen with black
     screen.blit(play_button, play_button_rect)  
+    
     
     
     
