2829920ee0451e2d77f2a795cc72ceb57aa6e9c6
Space <ashrayvarughese@gmail.com>
2025-08-25 20:12:09 +0530

    pause

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ pygame.mixer.init()
 screen_w,screen_h = 800, 600
 screen = pygame.display.set_mode((screen_w, screen_h))
 
-# image load
-# music paly button
+# * image load
+# play button
 play_button = pygame.image.load("assets/play.png").convert_alpha()
 play_button = pygame.transform.scale(play_button, (90, 80))  # Scale the image to desired size
 play_button_rect = play_button.get_rect(x=100, y=100 )
 
+pause_button = pygame.image.load("assets/pause.png").convert_alpha()
+pause_button = pygame.transform.scale(pause_button, (90, 80))  # Scale
+pause_button_rect = pause_button.get_rect(x=200, y=100 )
 # music load
 loaded_music = pygame.mixer.Sound("music/test.mp3")
 
@@ -24,12 +27,13 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if play_button_rect.collidepoint(event.pos):
                 loaded_music.play(-1)
+            if pause_button_rect.collidepoint(event.pos):
+                loaded_music.stop()
             
     mos_pos = pygame.mouse.get_pos()
     screen.fill((250, 238, 238))  # Fill the screen with black
-    screen.blit(play_button, play_button_rect)  
-    
-    
+    screen.blit(play_button, play_button_rect)
+    screen.blit(pause_button, pause_button_rect)
     
     
     pygame.display.flip()   # Update the full display Surface to the screen
