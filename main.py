457f76c53fcbbf73bd92d0fe0d6664195c4328e6
Space <ashrayvarughese@gmail.com>
2025-08-29 21:57:58 +0530

    many qualty cahnges

--- main.py
+++ main.py
@@ -7,8 +7,17 @@ pygame.mixer.set_num_channels(1)
 screen_w,screen_h = 800, 600
 screen = pygame.display.set_mode((screen_w, screen_h))
 font = pygame.font.Font(None, 50)
+pygame.display.set_caption("Music Player")
 
+# hover text
+def hover_text_func(text_name, x_cor, y_cor):
+    hover_font = pygame.font.Font(None, 25)
+    hover_text_name = text_name
+    hover_text = hover_font.render(hover_text_name, True, (0, 0, 0))
 
+    hover_text_rect = hover_text.get_rect(x=x_cor, y=y_cor)
+    screen.blit(hover_text, hover_text_rect)
+    
 # * image load
 # play button
 play_button = pygame.image.load("assets/play.png").convert_alpha()
@@ -41,15 +50,8 @@ while running:
     mos_pos = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-        
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if play_button_rect.collidepoint(event.pos):
-                loaded_music.play(-1)
-            if pause_button_rect.collidepoint(event.pos):
-                loaded_music.stop()
+            running = False  
     
-    # screen.render.
 
     screen.fill((250, 238, 238))  # Fill the screen with black
     screen.blit(play_button, play_button_rect)
@@ -59,17 +61,40 @@ while running:
     screen.blit(text, text_rect)
     screen.blit(choose_button, choose_button_rect)
     
-    if choose_button_rect.collidepoint(mos_pos):
+    if play_button_rect.collidepoint(mos_pos):
+        hover_text_func("Play music", 100, 200)
         if pygame.mouse.get_pressed()[0]:  # Left mouse button is pressed
-            music = askopenfilename(title="select music file")
-            loaded_music = pygame.mixer.Sound(music).convert()
-            
-            music = music.removesuffix('.mp3')
-            music = music.split("/", 1)[1]
-            print(music)
-            music_name = font.render(music, True, (0, 0, 0))
-            music_name_rect = music_name.get_rect( x=0, y=400)
-            
-            
+            loaded_music.play(-1)
+    
+    if pause_button_rect.collidepoint(mos_pos):
+        hover_text_func("Pause music", 200, 200)
+        if pygame.mouse.get_pressed()[0]:  # Left mouse button is pressed
+             loaded_music.stop()
+    
+    if choose_button_rect.collidepoint(mos_pos):
+        hover_text_func("Choose music", 300, 200)
+        if pygame.mouse.get_pressed()[0]:  # Left mouse button is pressed
+            try:
+                music = askopenfilename(title="select music file", filetypes=[("MP3 files", "*.mp3")])
+                loaded_music = pygame.mixer.Sound(music)
+                    
+                print(music)
+                music = music.removesuffix('.mp3')
+                music = music.split("/", 1)[1]
+                print(music)
+                music_name = font.render(music, True, (0, 0, 0))
+                music_name_rect = music_name.get_rect( x=0, y=400)
+            except FileNotFoundError:
+                music = "music/swimming by flawedMangoes.mp3"
+                loaded_music = pygame.mixer.Sound(music)
+                    
+                print(music)
+                music = music.removesuffix('.mp3')
+                music = music.split("/", 1)[1]
+                print(music)
+                music_name = font.render(music, True, (0, 0, 0))
+                music_name_rect = music_name.get_rect( x=0, y=400)
+                    
+    # print(mos_pos)        
     pygame.display.flip()   # Update the full display Surface to the screen
 pygame.quit()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Media player using pygame
+This is made for my fun little project,
+I was really engaged with this even this is really small.
